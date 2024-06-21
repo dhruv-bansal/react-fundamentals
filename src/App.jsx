fca8591a8705276fd81e4c3a680eaeecd61d1f80
Dhruv <fracon@Dhruvs-MacBook-Pro-2.local>
2024-06-21 15:59:59 -0700

    using state and hooks to update UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-
-import { CORE_CONCEPTS } from "./data"; // this is an named import
+import { CORE_CONCEPTS, EXAMPLES } from "./data"; // this is an named import
 import Header from "./components/Header";
 import CoreConcepts from "./components/CoreConcept";
-
-
-
+import TabButton from "./components/TabButton";
+import { useState } from "react";
 
 function App() {
+const [selectedTopic, setSelectedTopic] = useState('components');
+
+  function handleClick(clickedButton) {
+    setSelectedTopic(clickedButton);
+    console.log(`You clicked on the button! ` + selectedTopic);
+  }
+
   return (
     <div>
       <Header />
@@ -29,20 +34,29 @@ function App() {
             <CoreConcepts
               {...CORE_CONCEPTS[2]} // this is a spread operator
             />
-             <CoreConcepts
-              {...CORE_CONCEPTS[2]} // this is a spread operator
+            <CoreConcepts
+              {...CORE_CONCEPTS[3]} // this is a spread operator
             />
           </ul>
-          <ul>
-            {CORE_CONCEPTS.map((concept, index) => (
-              <CoreConcepts
-                key={index}
-                title={concept.title}
-                description={concept.description}
-                image={concept.image}
-              />
-            ))}
-          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleClick('components')}>Component</TabButton>
+            <TabButton onSelect={() => handleClick('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleClick('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleClick('state')}>State</TabButton>
+          </menu>
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+              {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
+          {selectedTopic}
         </section>
       </main>
     </div>
