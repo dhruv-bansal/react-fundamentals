bfaadec5f4d56a5878d95871117913d0a4cc9219
Dhruv <fracon@Dhruvs-MacBook-Pro-2.local>
2024-07-09 14:48:44 -0700

    conditional css:

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,6 +1,7 @@
 //de structure the props object into children and other custom property of props i.e. onSelect
 export default function TabButton({children, onSelect, isSelected}) {
 
-    return <li><button className={isSelected ? "active" : undefined} onClick={onSelect}>{children}</button></li>
+    return <li><button className={isSelected ? "active" : undefined} onClick={onSelect}>{children}</button></li> 
+    //empty string is also valid apart from undefined
 
 }
