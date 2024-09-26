250fb9d8b4b6d8e4fab193259df6226857f95fe3
dhruv-bansal <dhruv.bansal@outlook.com>
2024-09-25 21:32:53 -0700

    adding log functionality in game

--- project-2-tic-tac-toe/src/components/Log.jsx
+++ project-2-tic-tac-toe/src/components/Log.jsx
@@ -1,7 +1,14 @@
-export default function Log() {
+export default function Log({turns}) {
 
     return (
         <ol id="log">
+            {
+                turns.map((turn, index) => (
+                    <li key={index}>
+                        {`${turn.player} played at row ${turn.square.row}, col ${turn.square.col}`}
+                    </li>
+                ))
+            }
         </ol>
     )
   
